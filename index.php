4ca578bd5ef3cbd54f3b4a9654e7c0f2703b980d
iyaaaan <christianocol12@gmail.com>
2021-01-08 15:53:11 +0800

    separated css files

--- index.php
+++ index.php
@@ -3,7 +3,44 @@
 <!--hello mundo-->
 
 <head>
-	<?php include('include-head.php'); ?>
+	<meta charset="UTF-8">
+
+	<meta content="width=device-width, initial-scale=1.0" name="viewport" />
+	<meta name="description" content="ArTine Photography is a team of expertly-trained photographers who captures the best shots for your special day. Artine Photography are open for Weddings, Birthdays, and Product Photography. They also have amazing paintings to decorate your home.">
+
+	<!-- favicon -->
+	<link rel="icon" href="img/favicon.png" />
+
+	<!-- External CSS File -->
+	<!--	<link rel="stylesheet" href="css/main.css">-->
+
+	<!-- Font Awesome 4.7.0 -->
+	<link rel="stylesheet" href="css/font-awesome.min.css">
+
+	<!-- Owl Carousel -->
+	<link rel="stylesheet" href="css/owl.carousel.min.css">
+	<link rel="stylesheet" href="css/owl.theme.default.min.css">
+
+	<!-- Fotorama -->
+	<link rel="stylesheet" href="css/fotorama.css">
+
+	<!-- LightGallery -->
+	<link rel="stylesheet" href="css/lightgallery.css">
+
+	<!-- AOS -->
+	<link rel="stylesheet" href="css/aos.css">
+
+	<!-- jQUery -->
+	<link rel="stylesheet" href="css/jquery-ui.css">
+
+	<link rel="stylesheet" href="css/base.css">
+	<link rel="stylesheet" href="css/animations.css">
+	<link rel="stylesheet" href="css/components.css">
+	<link rel="stylesheet" href="css/buttons.css">
+	<link rel="stylesheet" href="css/modal.css">
+	<link rel="stylesheet" href="css/layout.css">
+	<link rel="stylesheet" href="css/forms.css">
+	<link rel="stylesheet" href="css/home.css">
 
 	<title>A r t i N e | Photography</title>
 </head>
